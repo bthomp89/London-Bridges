72d3a422f24fac2e31493d9124b3660c5f6e17dc
Brayden Thompson <bthomp89@uwo.ca>
2022-11-15 13:51:36 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", express.static("static"));
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
