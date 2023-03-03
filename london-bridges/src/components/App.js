f3b2faebfc2ba05d0cc25aef194ca44e14c61265
surelym <yzhon64@uwo.ca>
2023-03-03 00:25:19 -0500

    teamPageUpdate1

--- london-bridges/src/components/App.js
+++ london-bridges/src/components/App.js
@@ -1,5 +1,6 @@
 import History from "./History/History";
 import Home from "./Home/Home";
+import Team from "./Team/Team";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/history" element={<History />} />
+          <Route path="/team" element={<Team />} />
         </Routes>
       </Router>
     </div>
