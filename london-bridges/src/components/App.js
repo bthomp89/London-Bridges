1e2b2e0c62d39051e057ddb6fbc3790c7a5281c0
LucasHartman <lucas.hartman@kognitive.ca>
2023-03-19 21:43:15 -0400

    Create navbar

--- london-bridges/src/components/App.js
+++ london-bridges/src/components/App.js
@@ -1,15 +1,40 @@
 import History from "./History/History";
 import Home from "./Home/Home";
 import Team from "./Team/Team";
+import "./styles.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+
+function isActive(page) {
+    if (window.location.href.includes(page)) return true;
+}
 
 function App() {
+    const [open, setOpen] = useState(false);
+
+    function toggleMobileNav() {
+        setOpen((prev) => !prev)
+    }
+
   return (
     <div>
+        <div class="navbar">
+            <div class="logo"></div>
+            <nav className={`links-container ${open && "active"}`}>
+                <a href="/home" className={isActive('home') ? 'active' : ''} onClick={toggleMobileNav}>HOME</a>
+                <a href="/history" className={isActive('history') ? 'active' : ''} onClick={toggleMobileNav}>HISTORY</a>
+                <a href="/team" className={isActive('team') ? 'active' : ''} onClick={toggleMobileNav}>OUR TEAM</a>
+                <a href="/events" className={isActive('events') ? 'active' : ''} onClick={toggleMobileNav}>EVENTS</a>
+            </nav>
+            <button className={`hamburger ${open && "active"}`} onClick={toggleMobileNav}></button>
+        </div>
+
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/history" element={<History />} />
           <Route path="/team" element={<Team />} />
         </Routes>
